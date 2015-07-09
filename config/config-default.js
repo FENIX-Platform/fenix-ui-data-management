88b8c2b673e10365e3549fbf18b9d9e597bb563a
danielefenix <daniele.salvatore@fao.org>
2015-07-09 11:20:27 +0200

    BUG FIX: data mng filter

--- config/config-default.js
+++ config/config-default.js
@@ -9,11 +9,10 @@ define(function () {
             active: "createdataset"
         },
 
-        SECONDARY_MENU : {
-            url : './submodules/fenix-ui-data-management/config/secondary_menu.json',
+        SECONDARY_MENU: {
+            url: './submodules/fenix-ui-data-management/config/secondary_menu.json',
             disable: ['delete', 'close', 'data', 'dsd']
         },
-
 
         DSD_EDITOR_CONTEXT_SYSTEM: 'demo1',
         DSD_EDITOR_DATASOURCES: ['D3S'],
@@ -22,10 +21,9 @@ define(function () {
         DSD_EDITOR_CODELISTS: "config/submodules/DSDEditor/CodelistsUAE.json",
 
         DATA_MANAGEMENT_NOT_LOGGEDIN_URL: "./index.html",
-        FAKE_AUTHENTICATION : true,
+        FAKE_AUTHENTICATION: true,
 
         CATALOG_BLANK_FILTER: 'config/submodules/catalog/uae-catalog-blank-filter.json',
-
 
         METADATA_EDITOR_GUI: "./submodules/fenix-ui-metadata-editor/conf/json/fx-editor-gui-config.json",
         METADATA_EDITOR_VALIDATION: "./submodules/fenix-ui-metadata-editor/conf/json/fx-editor-validation-config.json",
@@ -34,14 +32,11 @@ define(function () {
         METADATA_EDITOR_AJAX_EVENT_CALL: "./submodules/fenix-ui-metadata-editor/conf/json/fx-editor-ajax-config_DEV.json",
         METADATA_EDITOR_DATES: "./submodules/fenix-ui-metadata-editor/conf/json/fx-editor-dates-config.json",
 
-
-        //BASE_ADDRESS: "http://fenix.fao.org/d3s_dev/msd",
-        //BASE_ADDRESS: "http://fenix.fao.org/d3s/msd",
         SERVICE_BASE_ADDRESS: "http://fenix.fao.org/d3s_fenix/msd",
-        SERVICE_GET_DATA_METADATA: { service: "resources", queryParams: { full: true, dsd: true } },
-        SERVICE_SAVE_METADATA: { service: "resources/metadata" },
-        SERVICE_SAVE_DSD: { service: "resources/dsd" },
-        SERVICE_SAVE_DATA: { service: "resources" },
-        SERVICE_RESOURCES_FIND: { service: "resources/find" }
+        SERVICE_GET_DATA_METADATA: {service: "resources", queryParams: {full: true, dsd: true}},
+        SERVICE_SAVE_METADATA: {service: "resources/metadata"},
+        SERVICE_SAVE_DSD: {service: "resources/dsd"},
+        SERVICE_SAVE_DATA: {service: "resources"},
+        SERVICE_RESOURCES_FIND: {service: "resources/find"}
     };
 });
